357f81ee9fe21c7eed1940103d69b8c37c298752
mukesh Kumar <mukeshksingh242@gmail.com>
2024-03-19 14:48:47 +0530

    first commit

--- src/test/java/com/org/base/BaseClass.java
+++ src/test/java/com/org/base/BaseClass.java
@@ -2,7 +2,6 @@ package com.org.base;
 
 import java.time.Duration;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -11,39 +10,36 @@ import org.testng.annotations.BeforeClass;
 
 import com.org.utilites.ProjectConfig;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BaseClass {
-   
+
 	public static WebDriver driver;
-	ProjectConfig projectConfig=new ProjectConfig();
-	String url=projectConfig.getBaseUrl();
-	String browser=projectConfig.getBrowser();
-	
+	ProjectConfig projectConfig = new ProjectConfig();
+	String url = projectConfig.getBaseUrl();
+	String browser = projectConfig.getBrowser();
+
 	@BeforeClass
 	public void setUpbrowser() {
-		
-		switch(browser.toLowerCase()) {
-		
+
+		switch (browser.toLowerCase()) {
+
 		case "chrome":
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
+
+			driver = new ChromeDriver();
 			break;
-			
+
 		case "msedge":
-			
-			WebDriverManager.edgedriver().setup();
-			driver=new EdgeDriver();
+
+			driver = new EdgeDriver();
 			break;
 		case "firefox":
-			WebDriverManager.firefoxdriver().setup();
-			driver=new FirefoxDriver();
+
+			driver = new FirefoxDriver();
 			break;
 		default:
-			driver=null;
+			driver = null;
 			break;
 		}
-		
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
 		driver.get(url);
